1af6d36631715ed48cbba68e784ae805fbdedac1
Ewan MARTIN <ewan.martin@vigicorp.fr>
2019-04-15 17:42:07 +0200

    Close poll + reduce image size

--- src/Controller/PollController.php
+++ src/Controller/PollController.php
@@ -12,12 +12,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PollController extends AbstractController
 {
-    /**
-     * @Route("/poll", name="poll")
-     * @param \App\Repository\CharacterRepository $characterRepository
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
+//    /**
+//     * @Route("/poll", name="poll")
+//     * @param \App\Repository\CharacterRepository $characterRepository
+//     *
+//     * @return \Symfony\Component\HttpFoundation\Response
+//     */
     public function poll(Request $request, CharacterRepository $characterRepository)
     {
         /** @var User $user */
